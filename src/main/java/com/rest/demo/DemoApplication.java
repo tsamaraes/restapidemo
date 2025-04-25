bd26eb66fbcdc5b6e9ee73d0acac7b83c895331e
Tsamara Esperanti Erwin <Tsamara.Esperanti@ibm.com>
2025-04-25 14:47:17 +0700

    webhook test 2

--- src/main/java/com/rest/demo/DemoApplication.java
+++ src/main/java/com/rest/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.rest.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
       
+// Test Application
 @SpringBootApplication
 public class DemoApplication {
 
